79f99b194fd139d47f1259d61a80a715e56696c4
Scott <scott@conversocial.com>
2020-04-22 08:00:29 +0100

    Adding basic explore views

--- carbon_credentials/urls.py
+++ carbon_credentials/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.index),
     path('upload/', views.UploadView.as_view(), name='upload'),
     path('explore/', views.explore, name='explore'),
+    path('explore/building/<int:building_id>', views.explore_building, name='explore_building'),
+    path('explore/meter/<int:meter_id>', views.explore_meter, name='explore_meter'),
     path('visualise/', views.visualise, name='visualise'),
     path('admin/', admin.site.urls),
 ]
--- carbon_credentials/views.py
+++ carbon_credentials/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View
-from django.http import HttpResponseRedirect
 
-from . import forms
-from . import csv_uploader
+from . import csv_uploader, forms, models
 
 
 def index(request):
@@ -27,7 +25,30 @@ class UploadView(View):
 
 
 def explore(request):
-    pass
+    context = {
+        'buildings': models.Building.objects.all().order_by('id')
+    }
+    return render(request, 'explore.html', context)
+
+
+def explore_building(request, building_id):
+    building = get_object_or_404(models.Building, pk=building_id)
+    meters = models.Meter.objects.filter(building=building).order_by('id')
+    context = {
+        'building': building,
+        'meters': meters
+    }
+    return render(request, 'explore_building.html', context)
+
+
+def explore_meter(request, meter_id):
+    meter = get_object_or_404(models.Meter, pk=meter_id)
+    readings = models.MeterReadings.objects.filter(meter=meter).order_by('reading_date_time')
+    context = {
+        'meter': meter,
+        'readings': readings
+    }
+    return render(request, 'explore_meter.html', context)
 
 
 def visualise(request):
